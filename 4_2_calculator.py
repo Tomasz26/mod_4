a30c2a1ef319e928b26d23f1a6b62987cf64f15f
Tomasz Mantur <tomasz.mantur26@gmail.com>
2024-12-02 21:19:38 +0100

    Working calculator i guess

--- 4_2_calculator.py
+++ 4_2_calculator.py
@@ -1,4 +1,3 @@
-import sys
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
@@ -11,7 +10,7 @@ while True:
         logging.debug("Proszę wpisać liczbę 1, 2, 3 lub 4 zgodnie z instrukcją, spróbuj ponownie.")
 
 def getvalue(user_input):
-    #Funkcja pobierania wartości od użytkownika
+    #Funkcja pobierania wartości od użytkownika dla odejmowania i dzielenia
     while True:
         try:
             value = float(input(user_input))
@@ -19,14 +18,44 @@ def getvalue(user_input):
         except:
             logging.debug("Program przyjmuje tylko liczby, spróbuj ponownie:")
 
-if operation == 1 or 2:
-    x = getvalue("Podaj składnik 1: ")
-    y = getvalue("Podaj składnik 2: ")
-    z = getvalue("Podaj składnik 3: ")
-        
+def getvalue13(user_input):
+    #Funkcja pobierania wartości od użytkownika dla dodawania i mnożenia
+    values = []
+    while True:
+        try:
+            value = input(user_input)
+            if value == 'N':
+                break
+            value = float(value)
+            values.append(value)
+        except:
+            logging.debug("Program przyjmuje tylko liczby, spróbuj ponownie:")
+    return values
+
+if operation == 1 or operation == 3:
+    xs = getvalue13("Wpisz kolejną wartość, jeśli chcesz zakończyć wpisz 'N':")        
 else:
     x = getvalue("Podaj składnik 1: ")
     y = getvalue("Podaj składnik 2: ")
+
+if operation == 1:
+    logging.info(f"Dodaję wszystkie podane przez Ciebie wartości: {xs}.")
+    wynik = sum(xs)
+elif operation == 3:
+    logging.info(f"Mnożę wszystkie podane przez Ciebie wartości: {xs}.")
+    wynik = 1
+    for v in xs:
+        wynik *= v
+elif operation == 2:
+    logging.info(f"Od {x} odejmuję {y}.")
+    wynik = x - y
+elif operation == 4:
+    logging.info(f"{x} dzielę przez {y}.")
+    wynik = x / y
+
+print(f"Wynik Twoich obliczeń to: {wynik}")
+
+
 
 
 
